18c55512029d5a8e95a1d37695e30cb943afa955
Spencer Moore <44985476+Smoore090@users.noreply.github.com>
2019-02-24 17:24:15 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.b9bbe6cd4937987f19a5d19cd783,
+  secret: process.env.c18176a4d9a9aa99ea49e610bbd
 };
